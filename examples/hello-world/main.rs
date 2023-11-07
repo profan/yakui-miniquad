7c12ee4052004f1443aa199a9958968005b6a0fc
profan <robinhubner@gmail.com>
2023-11-07 22:52:46 +0000

    run fmt

--- examples/hello-world/main.rs
+++ examples/hello-world/main.rs
@@ -1,53 +1,38 @@
 use miniquad::*;
+use yakui::{widgets::Pad, Color};
 use yakui_miniquad::*;
-use yakui::{Color, widgets::Pad};
 
 struct Stage {
-    yakui_mq: YakuiMiniQuad
+    yakui_mq: YakuiMiniQuad,
 }
 
 impl Stage {
     pub fn new(ctx: &mut GraphicsContext) -> Stage {
         let yakui_mq = YakuiMiniQuad::new(ctx);
-        Stage {
-            yakui_mq
-        }
+        Stage { yakui_mq }
     }
 }
 
 impl EventHandler for Stage {
-
     fn mouse_motion_event(&mut self, ctx: &mut Context, x: f32, y: f32) {
         self.yakui_mq.mouse_motion_event(ctx, x, y);
     }
 
-    fn mouse_button_down_event(
-            &mut self,
-            ctx: &mut Context,
-            button: MouseButton,
-            x: f32,
-            y: f32,
-        ) {
+    fn mouse_button_down_event(&mut self, ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
         self.yakui_mq.mouse_button_down_event(ctx, button, x, y);
     }
 
-    fn mouse_button_up_event(
-            &mut self,
-            ctx: &mut Context,
-            button: MouseButton,
-            x: f32,
-            y: f32,
-        ) {
+    fn mouse_button_up_event(&mut self, ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
         self.yakui_mq.mouse_button_up_event(ctx, button, x, y);
     }
 
     fn key_down_event(
-            &mut self,
-            ctx: &mut Context,
-            keycode: KeyCode,
-            keymods: KeyMods,
-            repeat: bool,
-        ) {
+        &mut self,
+        ctx: &mut Context,
+        keycode: KeyCode,
+        keymods: KeyMods,
+        repeat: bool,
+    ) {
         self.yakui_mq.key_down_event(ctx, keycode, keymods, repeat);
     }
 
@@ -59,13 +44,7 @@ impl EventHandler for Stage {
         self.yakui_mq.mouse_wheel_event(ctx, x, y);
     }
 
-    fn char_event(
-            &mut self,
-            ctx: &mut Context,
-            character: char,
-            keymods: KeyMods,
-            repeat: bool,
-        ) {
+    fn char_event(&mut self, ctx: &mut Context, character: char, keymods: KeyMods, repeat: bool) {
         self.yakui_mq.char_event(ctx, character, keymods, repeat);
     }
 
@@ -74,7 +53,6 @@ impl EventHandler for Stage {
     }
 
     fn update(&mut self, ctx: &mut Context) {
-
         self.yakui_mq.start(ctx);
 
         yakui::center(|| {
@@ -86,11 +64,9 @@ impl EventHandler for Stage {
         });
 
         self.yakui_mq.finish();
-
     }
 
     fn draw(&mut self, ctx: &mut Context) {
-
         ctx.begin_default_pass(Default::default());
 
         // draw some stuff before the UI?
@@ -102,9 +78,7 @@ impl EventHandler for Stage {
         ctx.end_render_pass();
 
         ctx.commit_frame();
-
     }
-
 }
 
 fn main() {
