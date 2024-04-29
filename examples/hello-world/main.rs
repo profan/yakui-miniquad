ef5f0781e361fbe2d5527a474441e482afe7064d
juh9870 <juh9870@ukr.net>
2024-04-29 23:37:09 +0300

    Update to Miniquad v0.4 (#1)
    * feat: upgraded to miniquad v0.4.0
    
    * feat: macroquad example
    
    * fix: fix emitting invalid unicode characters to yakui
    
    Caused by a miniquad bug, which emits charcters from Private Use Area whenever a non-character key is pressed
    
    * chore: remove unused `image` package from dev dependencies
    
    * refactor: removed external event handlers and macroquad demo
    
    Only `draw` method from event handler actually used context, and it is unlikely to get used directly without yakui-miniquad getting wrapped in a custom event handler (like in the example)
    
    * chore: fix and format

--- examples/hello-world/main.rs
+++ examples/hello-world/main.rs
@@ -1,59 +1,26 @@
+use std::ops::DerefMut;
+
+use miniquad::window::new_rendering_backend;
 use miniquad::*;
 use yakui::{widgets::Pad, Color};
+
 use yakui_miniquad::*;
 
 struct Stage {
+    ctx: Box<Context>,
     yakui_mq: YakuiMiniQuad,
 }
 
 impl Stage {
-    pub fn new(ctx: &mut GraphicsContext) -> Stage {
-        let yakui_mq = YakuiMiniQuad::new(ctx);
-        Stage { yakui_mq }
+    pub fn new(mut ctx: Box<Context>) -> Stage {
+        let yakui_mq = YakuiMiniQuad::new(ctx.deref_mut());
+        Stage { ctx, yakui_mq }
     }
 }
 
 impl EventHandler for Stage {
-    fn mouse_motion_event(&mut self, ctx: &mut Context, x: f32, y: f32) {
-        self.yakui_mq.mouse_motion_event(ctx, x, y);
-    }
-
-    fn mouse_button_down_event(&mut self, ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
-        self.yakui_mq.mouse_button_down_event(ctx, button, x, y);
-    }
-
-    fn mouse_button_up_event(&mut self, ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
-        self.yakui_mq.mouse_button_up_event(ctx, button, x, y);
-    }
-
-    fn key_down_event(
-        &mut self,
-        ctx: &mut Context,
-        keycode: KeyCode,
-        keymods: KeyMods,
-        repeat: bool,
-    ) {
-        self.yakui_mq.key_down_event(ctx, keycode, keymods, repeat);
-    }
-
-    fn key_up_event(&mut self, ctx: &mut Context, keycode: KeyCode, keymods: KeyMods) {
-        self.yakui_mq.key_up_event(ctx, keycode, keymods);
-    }
-
-    fn mouse_wheel_event(&mut self, ctx: &mut Context, x: f32, y: f32) {
-        self.yakui_mq.mouse_wheel_event(ctx, x, y);
-    }
-
-    fn char_event(&mut self, ctx: &mut Context, character: char, keymods: KeyMods, repeat: bool) {
-        self.yakui_mq.char_event(ctx, character, keymods, repeat);
-    }
-
-    fn resize_event(&mut self, ctx: &mut Context, width: f32, height: f32) {
-        self.yakui_mq.resize_event(ctx, width, height);
-    }
-
-    fn update(&mut self, ctx: &mut Context) {
-        self.yakui_mq.start(ctx);
+    fn update(&mut self) {
+        self.yakui_mq.start();
 
         yakui::center(|| {
             yakui::colored_box_container(Color::CORNFLOWER_BLUE, || {
@@ -66,23 +33,55 @@ impl EventHandler for Stage {
         self.yakui_mq.finish();
     }
 
-    fn draw(&mut self, ctx: &mut Context) {
-        ctx.begin_default_pass(Default::default());
+    fn draw(&mut self) {
+        self.ctx.begin_default_pass(Default::default());
 
         // draw some stuff before the UI?
 
-        self.yakui_mq.draw(ctx);
+        self.yakui_mq.draw(self.ctx.deref_mut());
 
         // ... draw some stuff after the UI!
 
-        ctx.end_render_pass();
+        self.ctx.end_render_pass();
 
-        ctx.commit_frame();
+        self.ctx.commit_frame();
+    }
+
+    fn resize_event(&mut self, width: f32, height: f32) {
+        self.yakui_mq.resize_event(width, height);
+    }
+
+    fn mouse_motion_event(&mut self, x: f32, y: f32) {
+        self.yakui_mq.mouse_motion_event(x, y);
+    }
+
+    fn mouse_wheel_event(&mut self, x: f32, y: f32) {
+        self.yakui_mq.mouse_wheel_event(x, y);
+    }
+
+    fn mouse_button_down_event(&mut self, button: MouseButton, x: f32, y: f32) {
+        self.yakui_mq.mouse_button_down_event(button, x, y);
+    }
+
+    fn mouse_button_up_event(&mut self, button: MouseButton, x: f32, y: f32) {
+        self.yakui_mq.mouse_button_up_event(button, x, y);
+    }
+
+    fn char_event(&mut self, character: char, keymods: KeyMods, repeat: bool) {
+        self.yakui_mq.char_event(character, keymods, repeat);
+    }
+
+    fn key_down_event(&mut self, keycode: KeyCode, keymods: KeyMods, repeat: bool) {
+        self.yakui_mq.key_down_event(keycode, keymods, repeat);
+    }
+
+    fn key_up_event(&mut self, keycode: KeyCode, keymods: KeyMods) {
+        self.yakui_mq.key_up_event(keycode, keymods);
     }
 }
 
 fn main() {
-    miniquad::start(conf::Conf::default(), |mut ctx| {
-        Box::new(Stage::new(&mut ctx))
+    miniquad::start(conf::Conf::default(), || {
+        Box::new(Stage::new(new_rendering_backend()))
     });
 }
